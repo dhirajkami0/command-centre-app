e6e6d30e3580570d71184d14b9d1152d2b31e320
dhirajkami0 <dhirajkami0@gmail.com>
2026-04-24 23:14:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    🔥 GREENGUARD SERVICE WORKER (SMART UPDATE)
 ========================================= */
 
-const CACHE_NAME = "greenguard-v23"; // 🔥 CHANGE EVERY UPDATE
+const CACHE_NAME = "greenguard-v24"; // 🔥 CHANGE EVERY UPDATE
 
 const APP_SHELL = [
   "./",
